c0f67ca95e9ccd15fbd4f14f21793a39da5ddbfc
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-01-13 14:49:31 -0500

    adding bootstrap piece by piece

--- 4roles/src/components/Inventory.js
+++ 4roles/src/components/Inventory.js
@@ -7,10 +7,10 @@ class Inventory extends React.Component {
             <div className = "inventory" >
                 <h4 className="InventoryText">Inventory</h4>
                 <div id = "inventory">
+                    <div className="row">
                     <Item />
-                    <Item />
-                    <Item />
-                </div> 
+                    </div> 
+                </div>
             </div>
         );
     }
--- 4roles/src/components/Item.js
+++ 4roles/src/components/Item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class Item extends React.Component {
-  render() {
-    return (
-      <div className="item">
-      </div>
-    );
-  }
+    render() {
+        return ( < div className = "item col-sm-1" >
+            < img src = "" / >
+            < /div>
+        );
+    }
 }
 
 export default Item;
